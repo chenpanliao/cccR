5810afb369878f386cd1b76ec39ec452ab97ab54
Chen-Pan Liao <andrew.43@gmail.com>
2014-01-07 17:39:25 +0800

    Update interp1.R

--- source/R/interp1.R
+++ source/R/interp1.R
@@ -2,10 +2,6 @@ interp1 <- function(
 	x,
 	y,
 	xout,
-	xmin = min(x),
-	xmax = max(x),
-	ymin = min(x),
-	ymax = max(x),	
 	method = c("linear", "fmm", "natural")
 ){
 	if (method[1] == "linear"){
